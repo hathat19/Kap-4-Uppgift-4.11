edd40a0c66264cd8acdf3edb489dcc6dbac0e970
humy06001 <humy06001@E-PW0234KS>
2023-10-21 21:22:22 +0200

    Add project files.

---
+++ Kap 4 Uppgift 4.11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Uppgift4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in något slumpmässigt:");
+            string userInput = Console.ReadLine();
+
+            bool again = false;
+
+            for (int i = 0; i < userInput.Length; i++) 
+            { 
+                //Aktuellt tecken == userInput[i]
+
+                //Finns tecknet igen?
+                for (int j = 0; j < userInput.Length; j++)
+                {
+                    if (j != i && userInput[j] == userInput[i]) //Om j INTE == indextecken som i men samma tecken
+                    {
+                            Console.WriteLine($"'{userInput[j]}' förekommer mer än en gång.");
+                            again = true; //Ja, det finns minst ett dubbeltecken
+                            break;
+                    }
+                }
+            }
+
+            if (again == false)
+            {
+                Console.WriteLine("Inget tecken förekommer fler än en gång.");
+            }
+            /*Jag hittade inget sätt att spara vilken karaktär som redan har funnits av programmet, 
+            så den skriver att samma karaktär har hittats flera gånger.*/
+        }
+    }
+}
